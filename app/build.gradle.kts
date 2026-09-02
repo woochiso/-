6388fec9b6859e3f092944b12f58d5548e6415e0
woochiso <malttugi001@gmail.com>
2026-09-03 04:06:24 +0900

    Android versionCode 3으로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     applicationId = "com.aistudio.emotiondiary.xrqvzp"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
+    versionCode = 3
     versionName = "1.1"
 
     buildConfigField("String", "WOOCHISO_API_BASE_URL", "\"https://woochiso.com/api/\"")
